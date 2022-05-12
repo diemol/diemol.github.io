ccf7a8c3993fd56d882102a9352aa25b1ccfc200
Diego Molina <diemol@gmail.com>
2022-05-12 14:28:48 +0200

    Updating site

--- build-site.sh
+++ build-site.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeleting Hugo previously generated directories...\033[0m"
-rm -rf diemol/public
+rm -rf public
 
 echo -e "\033[0;32mGenerating Hugo site...\033[0m"
-cd diemol && hugo && cd ..
-
-echo -e "\033[0;32mRefreshing contents of the published directory...\033[0m"
-rm -rf $(ls diemol/public)
-mv diemol/public/* .
+hugo
 
 echo -e "\033[0;32mDone building site!\033[0m"
