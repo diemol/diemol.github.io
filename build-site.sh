b4f66cbd90efc93a55f2fc99ddec113361c3d9ae
Diego Molina <diemol@gmail.com>
2022-05-12 14:30:59 +0200

    Using docs as directory

--- build-site.sh
+++ build-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeleting Hugo previously generated directories...\033[0m"
-rm -rf public
+rm -rf docs
 
 echo -e "\033[0;32mGenerating Hugo site...\033[0m"
-hugo
+hugo --destination docs
 
 echo -e "\033[0;32mDone building site!\033[0m"
