b1315f74c339194b6ba5f83b4a74bdcd4bcb7bca
Diego Molina <diemol@gmail.com>
2024-02-19 12:23:46 +0100

    Adding CNAME

--- build-site.sh
+++ build-site.sh
@@ -6,4 +6,7 @@ rm -rf docs
 echo -e "\033[0;32mGenerating Hugo site...\033[0m"
 hugo --destination docs
 
+echo -e "\033[0;32mCopying CNAME...\033[0m"
+cp CNAME docs
+
 echo -e "\033[0;32mDone building site!\033[0m"
